9089e9353910a052777f08125b0ef8042bb27f6b
Ben Dapkiewicz <ben@instituteofplay.org>
2014-03-05 13:39:12 -0800

    Included a build for uploading the existing bayes models to Couchbase Will be a better system for interpreting these models instead of hardcoding the document

---
+++ build/upload_models.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Navigate to the location of the class file
+cd ../classes
+
+REM Run the program with the necessary jar files
+java -cp .;../lib/couchbase-client-1.3.2.jar;../lib/spymemcached-2.10.5.jar;../lib/weka.jar;../lib/gson-2.2.4.jar;../lib/httpcore-4.3.jar;../lib/httpcore-nio-4.3.jar;../lib/jettison-1.1.jar;../lib/commons-codec-1.5.jar;../lib/netty-3.5.5.Final.jar UploadBayesDocument
+
+REM Navigate to the build directory
+cd ../build
