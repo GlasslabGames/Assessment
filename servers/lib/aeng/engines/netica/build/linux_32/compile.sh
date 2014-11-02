43897f5e1c4bf26a6cc334a93347e9654bda1683
Ben Dapkiewicz <ben@glasslabgames.org>
2014-11-01 18:53:57 -0700

    removed target 1.6

--- servers/lib/aeng/engines/netica/build/linux_32/compile.sh
+++ servers/lib/aeng/engines/netica/build/linux_32/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Compile the java programs and store the .class files in ../classes
-javac -target 1.6 -cp ../../lib/linux_32/NeticaJ.jar ../../src/SimpleBayes.java -d ../../classes
-javac -target 1.6 -cp ../../lib/linux_32/NeticaJ.jar ../../src/AdvancedBayes.java -d ../../classes
+javac -cp ../../lib/linux_32/NeticaJ.jar ../../src/SimpleBayes.java -d ../../classes
+javac -cp ../../lib/linux_32/NeticaJ.jar ../../src/AdvancedBayes.java -d ../../classes
