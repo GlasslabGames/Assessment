9e3a61dfe23876bfcf0e8d760bf9242fa682fe8d
Luke <luke@glasslabgames.org>
2015-04-10 13:56:15 -0700

    update reportCard data structure to track watch out progress, finish non watchout achievements, and begin adding some watch out rules in distiller.

--- servers/lib/aeng/engines/standards/games/AA-1/distiller.js
+++ servers/lib/aeng/engines/standards/games/AA-1/distiller.js
@@ -31,29 +31,26 @@ AA_Distiller.prototype.preProcess = function(sessionsEvents, currentResults)
 
     var action;
     var data;
-    var questId;
     var achievement;
     var standard;
     var conditions;
     var status;
     var ratio;
-    var weakness;
     var total;
     var good;
+    var tally;
 
     _(eventsList).forEach(function(event){
         action = event.action;
         data = event.data;
-        questId = data.questId || null;
-        weakness = data.weakness || null;
 
-        if(action === "Fuse_core" && weakness){
+        if(action === "Fuse_core" && data.weakness){
             standard = "CCRA.R.1";
             conditions = reportCard[standard];
             status = conditions.status;
             if(status !== "Full"){
                 total = ++conditions.data.totalFuseCores;
-                if(weakness === "none"){
+                if(data.weakness === "none"){
                     good = ++conditions.data.strongFuseCores;
                 } else{
                     good = conditions.data.strongFuseCores;
@@ -67,7 +64,91 @@ AA_Distiller.prototype.preProcess = function(sessionsEvents, currentResults)
             }
         }
 
-        if(questId && eventStandardsMap[action] && eventStandardsMap[action][questId]){
+        if(action === "Launch_attack" && data.success !== undefined){
+            standard = "21st.RE";
+            conditions = reportCard[standard];
+            status = conditions.status;
+            if(status !== "Full"){
+                total = ++conditions.data.totalLaunchAttacks;
+                if(data.success === true){
+                    good = ++conditions.data.successLaunchAttacks;
+                } else{
+                    good = conditions.data.successLaunchAttacks;
+                }
+                ratio = good/total;
+                if(ratio < conditions.data.threshold){
+                    if(achievementsHierarchy["Watchout1"] > achievementsHierarchy[status]){
+                        conditions.status = "Watchout1";
+                    }
+                }
+            }
+        }
+
+        if(action === "Finish_battle" && data.success === false) {
+            if (data.quest === "Build a Bot\r\n") {
+                standard = "RI 6.8";
+                conditions = reportCard[standard];
+                status = conditions.status;
+                if (achievementsHierarchy[status] < achievementsHierarchy["Full"]) {
+                    tally = ++conditions.data.losses;
+                    if (tally > 2) {
+                        conditions.status = "Watchout2";
+                    }
+                }
+            } else if (data.quest === "Which Protein?") {
+                standards = "RI 7.8";
+                conditions = reportCard[standard];
+                status = conditions.status;
+                if (achievementsHierarchy[status] < achievementsHierarchy["Partial"]) {
+                    tally = ++conditions.data.losses1;
+                    if (tally > 2) {
+                        conditions.status = "Watchout2";
+                    }
+                }
+            } else if (data.quest === "Helpbots") {
+                standards = "RI 7.8";
+                conditions = reportCard[standard];
+                status = conditions.status;
+                if (achievementsHierarchy[status] < achievementsHierarchy["Full"]) {
+                    tally = ++conditions.data.losses2;
+                    if (tally > 2) {
+                        conditions.status = "Watchout2";
+                    }
+                }
+            } else if (data.quest === "Bot Trainer 5000") {
+                standards = "RI 8.8";
+                conditions = reportCard[standard];
+                status = conditions.status;
+                if (achievementsHierarchy[status] < achievementsHierarchy["Partial"]) {
+                    tally = ++conditions.data.losses1;
+                    if (tally > 2) {
+                        conditions.status = "Watchout1";
+                    }
+                }
+            } else if(data.quest === "Hero Or Zero"){
+                standards = "RI 8.8";
+                conditions = reportCard[standard];
+                status = conditions.status;
+                if (achievementsHierarchy[status] < achievementsHierarchy["Full"]) {
+                    tally = ++conditions.data.losses2;
+                    if (tally > 2) {
+                        conditions.status = "Watchout1";
+                    }
+                }
+            }
+            // still have a few watchouts to define
+            //else if(data.quest === "Let's Evo"){
+            //
+            //}
+        }
+        //if(achievementsHierarchy[status] < achievementsHierarchy["Partial"]){
+        //    tally = ++conditions.data.failures;
+        //    if(tally > 3){
+        //        conditions.status = "Watchout1";
+        //    }
+        //}
+
+        if(data.questId && eventStandardsMap[action] && eventStandardsMap[action][data.questId]){
             achievement = eventStandardsMap[action][questId];
             standard = achievement[0];
             achievement = achievement[1];
@@ -79,7 +160,7 @@ AA_Distiller.prototype.preProcess = function(sessionsEvents, currentResults)
                     conditions.status = achievement;
                 } else{
                     ratio = _findThresholdRatio(conditions, standard, achievement);
-                    if(ratio > conditions.data.threshold){
+                    if(ratio >= conditions.data.threshold){
                         conditions.status = achievement;
                     } else{
                         conditions.status = "Watchout2";
@@ -100,12 +181,14 @@ AA_Distiller.prototype.postProcess = function(distilled) {
 function _buildReportCardData(results, standards){
     var reportCard = {};
     if(_.isEmpty(results)) {
-        standards.forEach(function (standard) {
-            reportCard[standard] = {status: "Not-Started", data: {}};
-        });
-        reportCard["CCRA.R.1"].data = { totalFuseCores: 0, strongFuseCores: 0, threshold: 0.5};
+        reportCard["RI 6.8"] = { status: "Not-Started", data: { failures: 0, losses: 0} };
+        reportCard["RI 7.8"] = { status: "Not-Started", data: { losses1: 0, losses2:0} };
+        reportCard["RI 8.8"] = { status: "Not-Started", data: { losses1: 0, losses2:0} };
+        reportCard["CCRA.R.1"] = { status: "Not-Started", data: { totalFuseCores: 0, strongFuseCores: 0, threshold: 0.5} };
+        reportCard["CCRA.R.8"] = { status: "Not-Started", data: { losses: 0, failures: 0} };
         // this threshold is not determined in the google doc yet
-        reportCard["21st.RE"].data  = { totalLaunchAttacks: 0, successLaunchAttacks: 0, threshold: 0.5};
+        reportCard["21st.RE"]  = { status: "Not-Started", data: { totalLaunchAttacks: 0, successLaunchAttacks: 0, threshold: 0.5} };
+        reportCard["21st.MJD"] = { status: "Not-Started", data: { failures: 0, losses: 0} };
     } else {
         _.merge(reportCard, results);
     }
