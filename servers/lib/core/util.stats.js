59b66d560f96c4576b625965c32da63d596c6a7e
wcj <wcj@makingwonder.com>
2015-07-09 11:49:24 -0700

    log stats from Assessment

--- servers/lib/core/util.stats.js
+++ servers/lib/core/util.stats.js
@@ -23,14 +23,11 @@ function Stats(options, root){
         options
     );
 
-    /*
-    // disabled for now
     statsdInst = new StatsD(this.options.statsd);
     statsdInst.socket.on('error', function(err) {
         this.statsd = null;
         return console.error("StatsD: Error connecting to server. ", err);
     });
-    */
 
     this.sRoot = root;
     this.root  = root;
