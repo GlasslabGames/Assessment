91b202743cc2ebfcda109651e7506542ef93d245
wcj <wcj@makingwonder.com>
2015-11-03 04:51:06 -0800

    work in progress

--- servers/lib/aeng/engines/javascript/engine.js
+++ servers/lib/aeng/engines/javascript/engine.js
@@ -34,6 +34,7 @@ JavascriptEngine.prototype.run = function(userId, gameId, gameSessionId, eventsD
 return when.promise(function(resolve, reject) {
 // ------------------------------------------------
 
+/*
     // load file and run
     try {
         //console.log("AssessmentEngine: Javascript_Engine - getDistillerFunction cwd:", process.cwd());
@@ -46,6 +47,43 @@ return when.promise(function(resolve, reject) {
     } catch(err) {
         console.error("AssessmentEngine: Javascript_Engine - Get Distiller Function Error -", err);
         reject(err);
+    }
+*/
+
+    var g;
+    var file;
+    var game;
+    var got_game;
+
+    try {
+        file = this.engineDir + "games"+path.sep + gameId+".js";
+        game = require(file);
+        got_game = true;
+    } catch(err) {
+        got_game = false;
+    }
+
+    if(got_game) {
+
+        // load file and run
+        try {
+            //console.log("AssessmentEngine: Javascript_Engine - getDistillerFunction cwd:", process.cwd());
+            file = this.engineDir + "games"+path.sep + gameId+".js";
+            game = require(file);
+
+            g = new game(this, this.aeService, this.options);
+            g.process(userId, gameId, gameSessionId, eventsData).then(resolve, reject);
+
+        } catch(err) {
+            console.error("AssessmentEngine: Javascript_Engine - Get Distiller Function Error -", err);
+            reject(err);
+        }
+    } else {
+
+        // auto process SOWO events
+        _processAutoSOWOs(this, userId, gameId, gameSessionId, eventsData)
+        .then(resolve, reject);
+
     }
 
 // ------------------------------------------------
@@ -71,6 +109,138 @@ JavascriptEngine.prototype.awardBadge = function(userId, badgeId) {
             }
         }.bind(this) );
 }
+
+
+
+
+
+
+
+/*
+ Telemetry can directly trigger Shout Outs and Watch Outs.
+*/
+JavascriptEngine.prototype._processAutoSOWOs = function(that, userId, gameId, gameSessionId, eventsData) {
+
+    // function AW_SoWo(engine, aeService, options){ ...
+
+return when.promise(function (resolve, reject) {
+
+    var sql;
+    var dbFile = ':memory:';
+    var db = new sqlite3.Database(dbFile);
+
+    db.serialize(function () {
+
+        sql = "CREATE TABLE IF NOT EXISTS events (\
+            eventId INT, \
+            userId INT, \
+            gameSessionId TEXT, \
+            clientTimeStamp DATETIME, \
+            serverTimeStamp DATETIME, \
+            eventName TEXT, \
+            gameLevel TEXT, \
+            gameSessionEventOrder INT, \
+            eventData_Key TEXT, \
+            eventData_Value TEXT,\
+            target TEXT)";
+
+        db.run(sql);
+
+        // insert
+        sql = "INSERT INTO events ( \
+            eventId, \
+            userId, \
+            gameSessionId, \
+            clientTimeStamp, \
+            serverTimeStamp, \
+            eventName, \
+            gameLevel, \
+            gameSessionEventOrder, \
+            eventData_Key, \
+            eventData_Value, \
+            target \
+        ) VALUES (?, ?,?,?,?, ?,?,?, ?,?,?)";
+
+        var eventId = 0;
+        var totalNumEvents = 0;
+        var filterSoWoEventTypes = ['trigger_shout_out', 'trigger_watch_out'];
+        // var filterSoWoEventKeys = [
+        //     'so1', 'so2', 'so3', 'so4', 'so5', 'so6', 'so7', 'so8',
+        //     'so9', 'so10', 'so11', 'so12', 'so13', 'so14', 'so15', 'so16',
+        //     'wo1', 'wo2', 'wo3', 'wo4', 'wo5', 'wo6', 'wo7', 'wo8',
+        //     'wo9', 'wo10', 'wo11', 'wo12', 'wo13', 'wo14', 'wo15', 'wo16'
+        // ];
+
+        for (var i = 0; i < eventsData.length; i++) {
+
+            if (!eventsData[i].events) continue;    // skip if not events
+
+            totalNumEvents += eventsData[i].events.length;
+
+            for (var j = 0; j < eventsData[i].events.length; j++) {
+
+                if (!_.contains(filterSoWoEventTypes, eventsData[i].events[j].eventName)) continue;
+
+                for (var key in eventsData[i].events[j].eventData) {    // for all eventData
+
+                    // if (!_.contains(filterSoWoEventKeys, key)) continue;
+
+                    var value = eventsData[i].events[j].eventData[key];
+
+                    if(_.isObject(value)) { value = JSON.stringify(value); }
+                    if(!_.isString(value)) { value = value.toString(); }
+
+                    var row = [
+                        eventId,
+                        eventsData[i].userId,
+                        eventsData[i].gameSessionId,
+                        eventsData[i].events[j].clientTimeStamp,
+                        eventsData[i].events[j].serverTimeStamp,
+                        eventsData[i].events[j].eventName,
+                        eventsData[i].events[j].gameLevel || "",
+                        eventsData[i].events[j].gameSessionEventOrder || i,
+                        key,
+                        value,
+                        eventsData[i].events[j].eventData['target'] || ""
+                    ];
+                    db.run(sql, row);
+                }
+
+                ++eventId;
+            }
+        }
+
+
+
+
+
+
+
+    }.bind(this));
+    db.close();
+
+}.bind(this));
+
+
+
+    // eg.
+    //     resolve(
+    //         {
+    //             id:   "so5",
+    //             type: "shoutout",
+    //             total: total,
+    //             // overPercent: (total - threshold + 1)/(max - threshold + 1)
+    //             overPercent: (total - threshold)/(max)
+    //         }
+
+}
+
+
+
+
+
+
+
 
 /*
  Dump all the relivent events into an in memory SQLite DB
