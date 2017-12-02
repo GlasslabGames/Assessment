3d70c07b76db32a1d7236c6756cc6f578561fd1c
Kyle Costaggini <kcostaggini@kcostaggini-macbook.local>
2017-12-01 21:47:09 -0800

    TAR-12 Added new telemetry engine and distiller for Stanford games

---
+++ servers/lib/aeng/engines/stanford/distillers/stanford.js
@@ -0,0 +1,71 @@
+/**
+ * Teachable Agents Stanford Report Module
+ *
+ */
+
+var _       = require('lodash');
+var when    = require('when');
+var sqlite3 = require('sqlite3').verbose();
+
+module.exports = TA_Stanford;
+
+function TA_Stanford(engine, aeService, options, aInfo) {
+    this.version = 0.1;
+    this.aeService = aeService;
+    this.options = _.merge({
+
+    }, options);
+
+    this.engine = engine;
+    this.aInfo = aInfo;
+}
+
+
+TA_Stanford.prototype.process = function(userId, gameId, gameSessionId, eventsData) {
+    var filterEventTypes = [
+        "currentMap"
+    ];
+    // always include one or more keys for a give type above
+    var filterEventKeys = [
+        //"mapQuality",   // currentMap
+        //"levelID",      // currentMap
+        //"classCode",    // currentMap
+        "map"           // currentMap
+    ];
+
+    return this.engine.processEventRules(userId, gameId, gameSessionId, eventsData, filterEventTypes, filterEventKeys, [
+	    this.collect_map_data.bind(this)
+    ])
+};
+
+TA_Stanford.prototype.collect_map_data = function(engine, db) {
+return when.promise(function(resolve, reject) {
+
+    var sql = 'SELECT * FROM events \
+        WHERE \
+            eventName="currentMap" \
+        ORDER BY \
+            serverTimeStamp ASC, gameSessionEventOrder ASC, eventData_Key ASC';
+
+    db.all(sql, function(err, results) {
+        if (err) {
+            console.error("AssessmentEngine: Stanford_Engine - TA_Stanford.collect_map_data DB Error:", err);
+            reject(err);
+            return;
+        }
+
+        var mapData = [];
+
+        for (var i=0; i < results.length; i++) {
+            var e = results[i];
+            
+            if (e.eventName == "currentMap" && e.eventData_Key == "map") {
+                mapData.push(e.eventData_Value);
+            }
+        }
+
+        resolve(mapData)
+    }.bind(this))
+
+}.bind(this));
+};
---
+++ servers/lib/aeng/engines/stanford/engine.js
@@ -0,0 +1,216 @@
+/**
+ * Engine Module for the Stanford Teachable Agents reports
+ *
+ * File created by kyle@concentricsky on Nov 28, 2017
+ */
+
+var fs = require('fs');
+var path = require('path');
+var child_process = require('child_process');
+var _ = require('lodash');
+var when = require('when');
+var Util;
+var util = require('util');
+var sqlite3 = require('sqlite3').verbose();
+var JavascriptEngine = require('../javascript/engine');
+
+//inherit from JavascriptEngine (which should probably be abstracted into a base and a 'sowo engine' added) -- Wiggins Oct2016
+
+function StanfordEngine(aeService, engineDir, options) {
+    // call super contructor
+    JavascriptEngine.apply(this, arguments);
+}
+util.inherits(StanfordEngine, JavascriptEngine);
+
+StanfordEngine.prototype.run = function(userId, gameId, gameSessionId, eventsData, aInfo){
+// add promise wrapper
+    return when.promise(function(resolve, reject) {
+// ------------------------------------------------
+
+        var g;
+        var file;
+        var game;
+        var got_game;
+        var distiller = aInfo.engine;
+        var distiller_path = this.engineDir + "distillers"+path.sep + distiller+".js";
+
+        try {
+            if (!distiller) {
+                got_game = false;
+            } else {
+                game = require(distiller_path);
+                got_game = true;
+            }
+        } catch(err) {
+            got_game = false;
+        }
+
+        // got_game = false;   // wip testing
+
+        if(got_game) {
+
+            // load file and run
+            try {
+                game = require(distiller_path);
+
+                g = new game(this, this.aeService, this.options, aInfo);
+                g.process(userId, gameId, gameSessionId, eventsData).then(resolve, reject);
+
+            } catch(err) {
+                console.error("AssessmentEngine: Stanford_Engine - Get Distiller Function Error -", err);
+                reject(err);
+            }
+        } else {
+
+            // auto process SOWO events
+            this._processAutoSOWOs(this, userId, gameId, gameSessionId, eventsData, aInfo)
+                .then(resolve, reject);
+        }
+
+// ------------------------------------------------
+    }.bind(this));
+// end promise wrapper
+};
+
+StanfordEngine.prototype.processEventRules = function(userId, gameId, gameSessionId, eventsData, filterEventTypes, filterEventKeys, ruleFuncs) {
+// add promise wrapper
+    return when.promise(function (resolve, reject) {
+// ------------------------------------------------
+        var dbFile = ':memory:';
+        var db = new sqlite3.Database(dbFile);
+        db.serialize(function () {
+            var sql;
+
+            sql = "CREATE TABLE IF NOT EXISTS events (\
+            eventId INT, \
+            userId INT, \
+            gameSessionId TEXT, \
+            clientTimeStamp DATETIME, \
+            serverTimeStamp DATETIME, \
+            eventName TEXT, \
+            gameLevel TEXT, \
+            gameSessionEventOrder INT, \
+            eventData_Key TEXT, \
+            eventData_Value TEXT,\
+            target TEXT)";
+            db.run(sql);
+
+            // insert
+            sql = "INSERT INTO events ( \
+            eventId, \
+            userId, \
+            gameSessionId, \
+            clientTimeStamp, \
+            serverTimeStamp, \
+            eventName, \
+            gameLevel, \
+            gameSessionEventOrder, \
+            eventData_Key, \
+            eventData_Value, \
+            target \
+        ) VALUES (?, ?,?,?,?, ?,?,?, ?,?,?)";
+
+            // check ahead if filterEventKeys contains "all"
+            var gatherAllEvents = false;
+            if( _.contains( filterEventKeys, "all" ) ) {
+                gatherAllEvents = true;
+            }
+
+            var eventId = 0;
+            var totalNumEvents = 0;
+            for (var i = 0; i < eventsData.length; i++) {
+                // skip if not events
+                if (!eventsData[i].events) continue;
+
+                totalNumEvents += eventsData[i].events.length;
+                for (var j = 0; j < eventsData[i].events.length; j++) {
+
+                    // only add events if in filter list
+                    if (!_.contains(filterEventTypes, eventsData[i].events[j].eventName)) continue;
+
+                    // for all eventData
+                    for (var key in eventsData[i].events[j].eventData) {
+
+                        // only add event data if in filter list
+                        if (!_.contains(filterEventKeys, key) && !gatherAllEvents) continue;
+
+                        var value = eventsData[i].events[j].eventData[key];
+                        // convert to string
+                        if(_.isObject(value)) {
+                            value = JSON.stringify(value);
+                        }
+                        if(!_.isString(value)) {
+                            value = value.toString();
+                        }
+
+                        // Print the data for debugging
+                        //console.log( eventId + " " + eventsData[i].gameSessionId + " " + eventsData[i].events[j].gameLevel + " " + eventsData[i].events[j].eventName + " " + key + " " + value );
+
+                        var row = [
+                            eventId,
+                            eventsData[i].userId,
+                            eventsData[i].gameSessionId,
+                            eventsData[i].events[j].clientTimeStamp,
+                            eventsData[i].events[j].serverTimeStamp,
+                            eventsData[i].events[j].eventName,
+                            eventsData[i].events[j].gameLevel || "",
+                            eventsData[i].events[j].gameSessionEventOrder || i,
+                            key,
+                            value,
+                            eventsData[i].events[j].eventData['target'] || ""
+                        ];
+                        db.run(sql, row);
+                    }
+
+                    // If the eventsData is empty and we're filtering for "all", add the event anyway
+                    if( gatherAllEvents && _.isEmpty( eventsData[i].events[j].eventData ) ) {
+                        var row = [
+                            eventId,
+                            eventsData[i].userId,
+                            eventsData[i].gameSessionId,
+                            eventsData[i].events[j].clientTimeStamp,
+                            eventsData[i].events[j].serverTimeStamp,
+                            eventsData[i].events[j].eventName,
+                            eventsData[i].events[j].gameLevel || "",
+                            eventsData[i].events[j].gameSessionEventOrder || i,
+                            "",
+                            "",
+                            ""
+                        ];
+                        db.run(sql, row);
+                    }
+
+                    eventId++;
+                }
+            }
+
+            if (this.options.env == "dev") {
+                console.log("AssessmentEngine: Stanford_Engine - processEventRules: process - # of events:", totalNumEvents);
+            }
+
+            var promiseList = [];
+            for (var i = 0; i < ruleFuncs.length; i++) {
+                // calling the function
+                if (_.isFunction(ruleFuncs[i])) {
+                    promiseList.push(ruleFuncs[i](this,db, userId, gameId, gameSessionId, eventsData));
+                }
+            }
+
+            when.all(promiseList)
+                .then(function (list) {
+                    resolve(list);
+                }.bind(this))
+
+                // catch all errors
+                .then(null, function (err) {
+                    reject(err);
+                }.bind(this));
+        }.bind(this));
+        db.close();
+
+// ------------------------------------------------
+    }.bind(this));
+// end promise wrapper
+};
+
+module.exports = StanfordEngine;
